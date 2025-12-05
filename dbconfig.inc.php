c3049813804f646e5bdfb1777485a367cc50623a
trankhanh23kod <trankhanh23kod@gmail.com>
2025-12-05 10:28:37 +0700

    Update dbconfig.inc.php

--- dbconfig.inc.php
+++ dbconfig.inc.php
@@ -1,35 +1,16 @@
 <?php
-/*
- * Copyright 2005-2016 OCSInventory-NG/OCSInventory-ocsreports contributors.
- * See the Contributors file for more details about them.
- *
- * This file is part of OCSInventory-NG/OCSInventory-ocsreports.
- *
- * OCSInventory-NG/OCSInventory-ocsreports is free software: you can redistribute
- * it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the License,
- * or (at your option) any later version.
- *
- * OCSInventory-NG/OCSInventory-ocsreports is distributed in the hope that it
- * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with OCSInventory-NG/OCSInventory-ocsreports. if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- */
 
-define("DB_NAME", "ocsweb");
-define("SERVER_READ","localhost");
-define("SERVER_WRITE","localhost");
-define("SERVER_PORT", 3306);
-define("COMPTE_BASE","ocs");
-define("PSWD_BASE","ocs");
+define("DB_NAME", getenv("DB_NAME"));     // MYSQLDATABASE
+define("SERVER_READ", getenv("DB_HOST")); // MYSQLHOST
+define("SERVER_WRITE", getenv("DB_HOST")); 
+define("SERVER_PORT", getenv("DB_PORT")); // MYSQLPORT
+
+define("COMPTE_BASE", getenv("DB_USER")); // MYSQLUSER
+define("PSWD_BASE", getenv("DB_PASS"));   // MYSQLPASSWORD
+
 define("ENABLE_SSL","0");
 define("SSL_MODE","");
-define("SSL_KEY","/path/to/client-key.pem");
-define("SSL_CERT","/path/to/client-cert.pem");
-define("CA_CERT","/path/to/ca.pem");
+define("SSL_KEY","");
+define("SSL_CERT","");
+define("CA_CERT","");
 ?>
